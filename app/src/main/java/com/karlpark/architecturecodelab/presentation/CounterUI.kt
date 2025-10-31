ca5463f8a96ccbe2e049bd894cb5c346fd88ed1f
Michael Hung Ling Jing <lj.michaelhung@gmail.com>
2025-11-01 03:08:10 +0800

    Add preview for counter ui

--- app/src/main/java/com/karlpark/architecturecodelab/presentation/CounterUI.kt
+++ app/src/main/java/com/karlpark/architecturecodelab/presentation/CounterUI.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -48,4 +49,14 @@ fun CounterUI(count: Int, onIncrement: () -> Unit, onDecrement: () -> Unit) {
 
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CounterUIPreview() {
+    CounterUI(
+        count = 0,
+        onIncrement = {},
+        onDecrement = {}
+    )
 }
